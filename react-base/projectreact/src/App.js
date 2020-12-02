dc88915e1528ebf02889cdc6dc325e877a3edf65
codelifee <suminjung09@gmail.com>
2020-12-02 16:38:36 +0900

    started react project

---
+++ react-base/projectreact/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tweet from "./Tweet";
+
+function App() {
+
+  return(
+    <div className="app">
+      <h1>Hello React</h1>
+      <Tweet />
+      <Tweet />
+      <Tweet />
+      <Tweet />
+    </div>
+  );
+}
+
+export default App;
